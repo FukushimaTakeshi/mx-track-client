66cd6c07d863dd8cb4cf879caa5c4cbb14bfa8f2
FukushimaTakeshi <whippet.818@gmail.com>
2021-05-13 22:34:35 +0900

    ダッシュボード用のテンプレート仮作成

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,17 +1,20 @@
 import React, { useContext } from 'react'
 import { auth } from './auth/auth'
 import { AuthContext } from './auth/AuthProvider'
+import { Dashboard } from './components/templates/Dashboard'
 
 const Home = () => {
   const { currentUser } = useContext(AuthContext)
 
   return (
-    <div>
-      {currentUser && currentUser.email}
+    <Dashboard>
+      <div>
+        {currentUser && currentUser.email}
 
-      <h2>Home Page</h2>
-      <button onClick={() => auth.auth().signOut()}>Sign out</button>
-    </div>
+        <h2>Home Page</h2>
+        <button onClick={() => auth.auth().signOut()}>Sign out</button>
+      </div>
+    </Dashboard>
   )
 }
 
