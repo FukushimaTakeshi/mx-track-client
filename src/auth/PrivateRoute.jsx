009750a712730dd23391e6de779652c792862cae
FukushimaTakeshi <whippet.818@gmail.com>
2021-07-07 00:50:21 +0900

    ローディング中アイコンを作成

--- src/auth/PrivateRoute.jsx
+++ src/auth/PrivateRoute.jsx
@@ -1,6 +1,16 @@
+import Backdrop from '@material-ui/core/Backdrop'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import { makeStyles } from '@material-ui/core/styles'
 import React, { useContext, useEffect, useState } from 'react'
 import { Redirect, Route } from 'react-router-dom'
 import { AuthContext } from './AuthProvider'
+
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#e6e6e6',
+  },
+}))
 
 const PrivateRoute = (props) => {
   const { currentUser, verifyUser } = useContext(AuthContext)
@@ -15,6 +25,8 @@ const PrivateRoute = (props) => {
     getUser()
   }, [])
 
+  const classes = useStyles()
+
   return authChecked ? (
     currentUser ? (
       <Route {...props} />
@@ -22,7 +34,9 @@ const PrivateRoute = (props) => {
       <Redirect to="/login" />
     )
   ) : (
-    <>ログイン処理中!!!</>
+    <Backdrop className={classes.backdrop} open={true}>
+      <CircularProgress color="inherit" />
+    </Backdrop>
   )
 }
 
