c128096668c978518aa19826e3650cc526df9bba
FukushimaTakeshi <whippet.818@gmail.com>
2021-09-16 21:05:04 +0900

    判定を少し修正

--- src/auth/Restricted.tsx
+++ src/auth/Restricted.tsx
@@ -9,10 +9,13 @@ type Props = {
 const Restricted: React.FC<Props> = ({ to, onlyAdministrator, children }) => {
   const { userRole } = useContext(AuthContext)
   const isAllowed = () => {
+    if (!userRole) {
+      return false
+    }
     if (onlyAdministrator) {
-      return userRole?.roles.includes('administrator')
+      return userRole.roles.includes('administrator')
     } else if (to) {
-      return userRole?.permissions.includes(to)
+      return userRole.permissions.includes(to)
     }
     return false
   }
