ece78245f420306ef016cfc7f17cf172cbfe8e96
FukushimaTakeshi <whippet.818@gmail.com>
2021-12-21 21:44:19 +0900

    Revert "Revert "[WIP] Suspenseを使用したAPIリクエストを仮で""
    This reverts commit a65997b0c0423d74ab6beadc3e8ed9b5567d571e.

--- src/components/MyPage.tsx
+++ src/components/MyPage.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import PracticeChart from './Chart/PracticeChart'
+import React, { Suspense } from 'react'
+import { apiClientWithAuth } from '../lib/api_client'
+import { Resource } from '../lib/resource'
+import PracticeChart, { IPracticeRecords } from './Chart/PracticeChart'
 import PracticeRecordList from './PracticeRecord/PracticeRecordList'
+import InnerLoading from './Spinner/InnerLoading'
 import { Dashboard } from './templates/Dashboard'
+import Title from './Title'
+
+const resource = new Resource(() =>
+  apiClientWithAuth.get<IPracticeRecords>(
+    '/practice_records/?sort=+practice_date&field=number_of_monthly'
+  )
+)
 
 const MyPage: React.FC = () => {
   return (
     <Dashboard>
-      <PracticeChart />
+      <Title>my activity</Title>
+      <Suspense fallback={<InnerLoading loading />}>
+        <PracticeChart resource={resource} />
+      </Suspense>
       <PracticeRecordList />
     </Dashboard>
   )
